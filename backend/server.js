92533c5ac4b4add8855780b786cf33ef928f8757
Harpreet-kaurr <kaurhap1995@gmail.com>
2025-10-07 13:09:33 -0400

    resolved

--- backend/server.js
+++ backend/server.js
@@ -23,15 +23,16 @@ app.use("/api/message", messageRoutes);
 
 // ----------------------- Deployment: Serve React Frontend -----------------------
 const __dirname1 = path.resolve();
+
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname1, "/frontend/build")));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"));
-  });
+  app.get("/*", (req, res) =>
+    res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"))
+  );
 } else {
   app.get("/", (req, res) => {
-    res.send("API is running successfully");
+    res.send("API is running..");
   });
 }
 
