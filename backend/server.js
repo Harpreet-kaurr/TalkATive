486da158ea845932e42a5e0649683d59b23d5d66
Harpreet-kaurr <kaurhap1995@gmail.com>
2025-10-07 13:14:38 -0400

    resolved

--- backend/server.js
+++ backend/server.js
@@ -27,9 +27,9 @@ const __dirname1 = path.resolve();
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname1, "/frontend/build")));
 
-  app.get("/*", (req, res) =>
-    res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"))
-  );
+   app.use((req, res) => {
+     res.sendFile(path.join(__dirname1, "frontend", "build", "index.html"));
+   });
 } else {
   app.get("/", (req, res) => {
     res.send("API is running..");
